688f35a0a86f63bd9fdbf65b6ddf69c2ba686f2d
Pelayo <pelayocanome@gmail.com>
2025-10-30 11:49:46 +0100

    crear bebida

--- model/Bebida.php
+++ model/Bebida.php
@@ -1,2 +1,13 @@
 <?php
 // Clase Bebida que hereda de Artículo
+require_once 'Articulo.php';
+
+class Bebida extends Articulo {
+    public $tamano;
+
+    public function __construct($nombre, $precio, $disponibilidad, $categoria, $tamano) {
+        parent::__construct($nombre, $precio, $disponibilidad, $categoria);
+        $this->tamano = $tamano;
+    }
+}
+?>
